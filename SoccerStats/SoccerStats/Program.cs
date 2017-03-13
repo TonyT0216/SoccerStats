3e5ae7814c7a063ec7e15292e1a9d076a8e32bfd
Tony Toscano <tonymichael16@gmail.com>
2017-03-13 13:47:43 -0400

    Added a ReadSoccerResults method that reads a file line by line

--- SoccerStats/SoccerStats/Program.cs
+++ SoccerStats/SoccerStats/Program.cs
@@ -13,23 +13,49 @@ namespace SoccerStats
         {
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
-            var fileName = Path.Combine(directory.FullName, "data.txt");
-            var file = new FileInfo(fileName);
+            var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
+            var fileContents = ReadSoccerResults(fileName);
+            // var file = new FileInfo(fileName);
             // If block will run if the file exists
-            if (file.Exists)
-                {
-                using (var reader = new StreamReader(file.FullName))
-                {
-                    Console.SetIn(reader);
-                    Console.WriteLine(Console.ReadLine());
-                }
-            }
+            //if (file.Exists)
+            //    {
+            //    using (var reader = new StreamReader(file.FullName))
+            //    {
+            //        Console.SetIn(reader);
+            //        Console.WriteLine(Console.ReadLine());
+            //    }
+            //}
+
+
             //var files = directory.GetFiles("*.txt");
             //foreach(var file in files)
             //{
             //    Console.WriteLine(file.Name);
             //}
 
+        }
+
+        public static string ReadFile(String fileName)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public static List<string[]> ReadSoccerResults(string fileName)
+        {
+            var soccerResults = new List<string[]>();
+            using (var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                while((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+                    soccerResults.Add(values);
+                }
+            }
+            return soccerResults;
         }
     }
 }
